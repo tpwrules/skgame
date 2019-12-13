a0fba803b06084c56b1ff5bc9be7f23bf4dff8c2
Thomas Watson <twatson52@icloud.com>
2019-12-12 21:03:46 -0600

    add crazy flip

--- the_game/AnimationPlayer.gd
+++ the_game/AnimationPlayer.gd
@@ -6,9 +6,18 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.current_animation = "sickflip"
 	pass # Replace with function body.
 
+var last = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.current_animation = "sickflip"
 	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if last:
+			self.current_animation = "crazyflip"
+	else:
+		self.current_animation = "sickflip"
+	last = not last

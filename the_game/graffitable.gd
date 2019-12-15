9b9e58e59728a38abfd5bdacb3b014239a3b6cde
Thomas Watson <twatson52@icloud.com>
2019-12-15 08:36:46 -0600

    improve graffiti efficiency

--- the_game/graffitable.gd
+++ the_game/graffitable.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
-# rectangles of all the graffitable areas
+# for every graffitable object:
+# rectangle representing its texture size
 var g_rect = []
-# and nodes we can use to draw on them
+# canvasitem we use to draw onto it
 var g_drawer = []
-# and queues of new sprays to add
+# queue of new sprays to add next frame
 var g_sprays = []
-# and the scale of each thing on the screen
-var g_scale = []
-# and the transform of it
+# size of spray circle
+var g_spray_size = []
+# transform from global coordinates to local coordinates
+# so we can draw the spray at the right place
 var g_transform = []
+# original texture so we can paint the object on the first frame
+var g_tex = []
 
 onready var sprayer = $"../../player/graphics/spray_pivot/sprayer"
 
+var SPRAY_SIZE = 20
+
+# any MeshInstance2Ds under us are object graphics and should be graffitable
 func find_graffitables(node):
 	var ours = []
 	for child in node.get_children():
@@ -24,69 +31,82 @@ func find_graffitables(node):
 
 func _ready():
 	var g_idx = 0
+	# transform each graffitable so we can draw on it
 	for graffitable in find_graffitables(self):
+		# get the original object texture
 		var tex = graffitable.get_texture()
-		var pos = graffitable.global_position#-self.global_position
-		var size = tex.get_size()#*graffitable.global_scale
+		# we have to keep the texture around so we can draw it to the new
+		# thing we're about to make on the first frame. if we don't reference
+		# it here, it will die by then.
+		tex.reference()
+		# size of the original object
+		var size = tex.get_size()
 		
-		# we want to make a texture we can draw random garbage on.
-		# we do this with a custom viewport.
+		# we can't just draw on an arbitrary texture. we instead create a
+		# viewport with an object we use for drawing
 		var viewport = Viewport.new()
 		viewport.size = tex.get_size()
 		viewport.usage = Viewport.USAGE_2D
+		# only clear the viewport initially, keep what we've sprayed after
 		viewport.render_target_clear_mode = Viewport.CLEAR_MODE_ONLY_NEXT_FRAME
 		viewport.render_target_v_flip = true
 		viewport.transparent_bg = true
 		
-		# a node2D is used for drawing operations
+		# a Node2D (or, well, a CanvasItem) is used for drawing operations
 		var drawer = Node2D.new()
 		viewport.add_child(drawer)
 		drawer.position = Vector2(0, 0)
 		drawer.scale = Vector2(1, 1)
 		# get notification when it's about to be drawn
-		drawer.connect("draw", self, "_on_draw", [g_idx])
-
-		# the garbage gets drawn masked by a mesh of the same shape as the actual texture
-		var tex_mesh = graffitable.get_mesh()
-		var vp_instance = MeshInstance2D.new()
-		vp_instance.set_mesh(tex_mesh)
-		vp_instance.set_texture(viewport.get_texture())
-		
-		graffitable.get_parent().add_child_below_node(graffitable, vp_instance)
-		graffitable.get_parent().add_child_below_node(graffitable, viewport)
-		vp_instance.global_position = graffitable.global_position
-		vp_instance.global_scale = graffitable.global_scale
-		vp_instance.global_rotation = graffitable.global_rotation
-		vp_instance.z_index = graffitable.z_index
-		vp_instance.z_as_relative = graffitable.z_as_relative
-
+		drawer.connect("draw", self, "_g_draw", [g_idx])
+		# instead of the original object, the graffitable now shows the viewport
+		graffitable.set_texture(viewport.get_texture())
+		# make sure we don't lose the viewport
+		graffitable.get_parent().add_child(viewport)
 		
 		g_rect.append(Rect2(Vector2(0, 0), size))
 		g_drawer.append(drawer)
 		g_sprays.append([])
-		g_scale.append(graffitable.global_scale)
+		var scale = graffitable.global_scale
+		if scale.x > scale.y:
+			g_spray_size.append(20/scale.x)
+		else:
+			g_spray_size.append(20/scale.y)
 		g_transform.append(graffitable.get_global_transform().translated(-size/2).affine_inverse())
-		g_idx += 1
+		g_tex.append([0, tex])
 
-var sprays = []
+		g_idx += 1
 
 func _physics_process(delta):
 	var spraying = Input.is_action_pressed("g_spray")
 	sprayer.set_spraying(spraying)
 	if not spraying: return
 	
-	var spray_pos = sprayer.global_position#-self.global_position
+	var spray_pos = sprayer.global_position
 	
 	for graffitable in range(len(g_rect)):
-		var rect = g_rect[graffitable]
-		var tex_pos = g_transform[graffitable]*spray_pos
-		if rect.has_point(tex_pos):
-			g_sprays[graffitable].append(tex_pos)
+		var g_pos = g_transform[graffitable]*spray_pos
+		if g_rect[graffitable].has_point(g_pos):
+			g_sprays[graffitable].append(g_pos)
 			g_drawer[graffitable].update()
 
-func _on_draw(g_idx):
+func _g_draw(g_idx):
+	
+	if g_tex[g_idx] != null:
+		var tex = g_tex[g_idx][1]
+		if g_tex[g_idx][0] == 0:
+			# draw the original object texture as the very first thing 
+			g_drawer[g_idx].draw_texture(tex, Vector2(0, 0))
+			# then free it next frame
+			g_tex[g_idx][0] = 1
+		elif g_tex[g_idx][0] == 1:
+			# free the texture now that it's been drawn
+			tex.unreference()
+			g_tex[g_idx] = null
 	if len(g_sprays[g_idx]) > 0:
-		var scale = g_scale[g_idx]
+		var drawer = g_drawer[g_idx]
+		var spray_size = g_spray_size[g_idx]
+		var color = Color(0, 0, 0, 1)
 		for spray in g_sprays[g_idx]:
-			g_drawer[g_idx].draw_circle(spray, 20/scale.x, Color(0, 0, 0, 1))
+			drawer.draw_circle(spray, spray_size, color)
 		g_sprays[g_idx] = []
